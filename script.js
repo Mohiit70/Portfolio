0cb666cdcca7eead665c4cf659a9d8945b98a1e6
Mohit Bisht <mail.mohitbisht@gmail.com>
2024-07-28 09:47:39 +0000

    added dark mode

--- script.js
+++ script.js
@@ -1,4 +1,26 @@
+const toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
+const currentTheme = localStorage.getItem('theme');
 const cursor = document.getElementById('cursor');
+
+if (currentTheme) {
+    document.documentElement.setAttribute('class', currentTheme);
+
+    if (currentTheme === 'dark-mode') {
+        toggleSwitch.checked = true;
+    }
+}
+
+function switchTheme(e) {
+    if (e.target.checked) {
+        document.documentElement.setAttribute('class', 'dark-mode');
+        localStorage.setItem('theme', 'dark-mode');
+    } else {
+        document.documentElement.setAttribute('class', '');
+        localStorage.setItem('theme', 'light-mode');
+    }
+}
+
+toggleSwitch.addEventListener('change', switchTheme, false);
 
 document.addEventListener('mousemove', (e) => {
     cursor.style.left = e.clientX + 'px';
